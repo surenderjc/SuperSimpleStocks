31991608c3536d186f142576cfd24f4359f46606
surenderjc <surender.jc@gmail.com>
2016-10-26 17:23:24 +0100

    SuperSimpleStock Initial commit
    JP Morgan Super simple stock code assignment code

---
+++ src/main/java/com/gbcorp/simple/stock/constants/SimpleStockConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.gbcorp.simple.stock.constants;
+
+/**
+ * @author user
+ *
+ */
+public class SimpleStockConstants {
+	
+	public static final String COMMON = "COMMON";
+
+}
